33aa5a6293c2822507316cdea0a561d7a80952aa
Michele Ferrante <michele.ferrante@protonmail.ch>
2022-04-03 19:34:07 +0200

    MD specific stuff

---
+++ after/ftplugin/markdown.lua
@@ -0,0 +1,9 @@
+-- 80 column hint
+vim.wo.colorcolumn = vim.wo.colorcolumn .. '80'
+
+
+-- set indentation
+vim.bo.tabstop = 2
+vim.bo.softtabstop = 2
+vim.bo.shiftwidth = 2
+vim.bo.expandtab = true
